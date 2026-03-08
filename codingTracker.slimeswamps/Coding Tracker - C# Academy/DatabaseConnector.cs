7d256f5b9af90bca7b06d9b6d3732c58a1d6f716
slimeswamps <slimeswamp.the.millionth@gmail.com>
2026-03-08 19:57:50 +0000

    separated records into another file

--- codingTracker.slimeswamps/Coding Tracker - C# Academy/DatabaseConnector.cs
+++ codingTracker.slimeswamps/Coding Tracker - C# Academy/DatabaseConnector.cs
@@ -119,12 +119,3 @@ class DatabaseConnector
         }
     }
 }
-
-public class Records()
-{
-    public int? TrackerID {  get; set; }
-    public string? StartTime { get; set; }
-    public string? EndTime { get; set; }
-    public string? Duration { get; set; }
-    public string? Date { get; set; }
-}
---
+++ codingTracker.slimeswamps/Coding Tracker - C# Academy/Records.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingTracker;
+
+internal class Records()
+{
+    public int? TrackerID { get; set; }
+    public string? StartTime { get; set; }
+    public string? EndTime { get; set; }
+    public string? Duration { get; set; }
+    public string? Date { get; set; }
+}
